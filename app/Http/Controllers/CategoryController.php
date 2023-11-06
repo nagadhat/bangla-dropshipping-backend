eaeba248ada5e87ce403be2f940037155a3c36c0
Razia Haque <raziahaque1990@gmail.com>
2023-11-06 17:22:03 +0600

    add documentation of category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,8 +9,29 @@ use File;
 
 class CategoryController extends Controller
 {
+    /**
+     * @OA\Get(
+     *      path="api/categories",
+     *      operationId="Category-index",
+     *      tags={"Category list"},
+     *      summary="list of Categories",
+     *      description="Returns list of categories",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *     )
+     */
     public function index(){
-
         $categories = Category::all();
         if($categories){
             return response()->json([
@@ -23,6 +44,45 @@ class CategoryController extends Controller
         }
 
     }
+
+    /**
+    * @OA\Post(
+    * path="/api/add/category",
+    * operationId="Stor Category",
+    * tags={"Category"},
+    * summary="Category Add",
+    * description="Adding new category here",
+    *     @OA\RequestBody(
+    *         @OA\JsonContent(),
+    *         @OA\MediaType(
+    *            mediaType="multipart/form-data",
+    *            @OA\Schema(
+    *               type="object",
+    *               required={"name"},
+    *               @OA\Property(property="name", type="text"),
+    *               @OA\Property(property="icon", type="file"),
+    *            ),
+    *        ),
+    *    ),
+    *      @OA\Response(
+    *          response=201,
+    *          description="Category Inserted Successfully",
+    *          @OA\JsonContent()
+    *       ),
+    *      @OA\Response(
+    *          response=200,
+    *          description="Category Inserted Successfully",
+    *          @OA\JsonContent()
+    *       ),
+    *      @OA\Response(
+    *          response=422,
+    *          description="Unprocessable Entity",
+    *          @OA\JsonContent()
+    *       ),
+    *      @OA\Response(response=400, description="Bad request"),
+    *      @OA\Response(response=404, description="Resource Not Found"),
+    * )
+    */
 
     public function store(Request $request){
 
@@ -38,9 +98,9 @@ class CategoryController extends Controller
 
         }else{
             $image = $request->file('icon');
-
+            // upload image to folder
             if( $request->has('icon')){
-                $image_name = rand(). $image->getClientOriginalName();
+                $image_name = time().rand().'.'. $image->getClientOriginalExtension();
                 $image_folder = public_path('/images/category-images');
                 $image->move($image_folder, $image_name);
 
